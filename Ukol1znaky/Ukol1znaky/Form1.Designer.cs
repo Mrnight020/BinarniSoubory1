51a023df38f2880227aec1dac9adf28413b09b96
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2023-03-22 20:51:11 +0100

    Add files via upload

---
+++ Ukol1znaky/Ukol1znaky/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Ukol1znaky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(166, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 109);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(134, 420);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Ukol1znaky/Ukol1znaky/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ukol1znaky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char predotaznikem = ' ';
+            int pozice = 0;
+            listBox1.Items.Clear();
+            FileStream data = new FileStream("znaky.dat",FileMode.Open,FileAccess.Read);
+            BinaryReader precti = new BinaryReader(data,Encoding.GetEncoding("UTF-8"));
+
+            precti.BaseStream.Position = 0;
+            while(precti.BaseStream.Position < precti.BaseStream.Length)
+            {
+                char znak = precti.ReadChar();
+                listBox1.Items.Add(znak);
+                if(znak == '?')
+                {
+                    pozice = (int)precti.BaseStream.Position;
+                    precti.BaseStream.Position -= 2;
+                    predotaznikem = precti.ReadChar();
+                    break;
+                }
+            }
+            MessageBox.Show("otaznik je na pozici : " + pozice + "\nznak pred otaznikem je : " + predotaznikem);
+
+            data.Close();
+        }
+    }
+}
